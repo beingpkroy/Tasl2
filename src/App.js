af7f1f262453eb37bafa0e7171d42bd7a7ba766c
beingpkroy <69778957+beingpkroy@users.noreply.github.com>
2020-09-21 16:07:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div class="grid-container">
+        <div class="grid-item1">Red</div>
+        <div class="grid-item"></div>
+        <div class="grid-item3">Green</div>
+        <div class="grid-item"></div>
+        <div class="grid-item5">Gray</div>
+        <div class="grid-item"></div>
+        <div class="grid-item7">Blue</div>
+        <div class="grid-item"></div>
+        <div class="grid-item9">Purple</div>
+      </div>
+    </div>
+  );
+}
